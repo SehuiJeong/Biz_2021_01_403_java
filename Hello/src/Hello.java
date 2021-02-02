ef34e7b222a9033066c2c5a567d98a332cfd8d90
SehuiJeong <Jsh6461123@gmail.com>
2021-02-02 14:24:27 +0900

    first

---
+++ Hello/src/Hello.java
@@ -0,0 +1,17 @@
+
+public class Hello {
+
+	public static void main (String args[]) {
+	System.out.println("반갑습니다");
+	System.out.println(30+40);
+	System.out.println(30*40);
+	System.out.println(30/40);
+	System.out.println(30-40);
+	System.out.println("이름 : 홀길동");
+	System.out.println("주소 : 서울시");
+	System.out.println("30" + "40");
+	System.out.println(30 + 40);
+	}
+	
+}
+
