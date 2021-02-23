40b6e5d87110030388e639b5901a913ed35cb59f
SehuiJeong <jsh6461123@gmail.com>
2021-02-23 12:54:51 +0900

    Array

--- Java_015/src/com/callor/applications/Array_04.java
+++ Java_015/src/com/callor/applications/Array_04.java
@@ -23,17 +23,6 @@ public class Array_04 {
 
 		int lastPosition = 0;
 		
-		/*
-		 * 변수는 앞에서 어떤 값을 저장했던 상관없이
-		 * 가장 마지막에 저장한 값만 가지고 있다(성질)
-		 */
-		int num = 0;
-		num = 100;
-		num = 101;
-		num = - 1;
-		num = - 1000;
-		System.out.println(num);
-		
 		for (int position = 0; position < intNums.length; position++) {
 			if (intNums[position] == num2) {
 				// 키보드로 입력한 값이 일치할 때마다
@@ -45,6 +34,19 @@ public class Array_04 {
 		// 가장 마지막으로 lastPosition = position이 실행된
 		// 결과만 담고 있다
 		System.out.println("가장 마지막으로 나타나는 위치 번호는\t" + lastPosition);
+		
+		// 번외 설명
+		/*
+		 * 변수는 앞에서 어떤 값을 저장했던 상관없이
+		 * 가장 마지막에 저장한 값만 가지고 있다(성질)
+		 */
+		int num = 0;
+		num = 100;
+		num = 101;
+		num = - 1;
+		num = - 1000;
+		System.out.println(num);
 	}
+
 
 }
